126d9ec2dcb5dcd04d302431146acd5a43be0631
Andrew Brighton <snatvb@ya.ru>
2023-01-30 23:12:00 +0200

    Init

--- next.config.js
+++ next.config.js
@@ -1,8 +1,26 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+    experimental: {
+        appDir: true,
+    },
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Cross-Origin-Opener-Policy',
+                        value: 'same-origin',
+                    },
+                    {
+                        key: 'Cross-Origin-Embedder-Policy',
+                        value: 'require-corp',
+                    },
+                ],
+            },
+        ]
+    },
 }
 
 module.exports = nextConfig
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,29 @@
 import './globals.css'
+import 'flowbite'
+import cx from 'classnames'
+import { IBM_Plex_Mono } from '@next/font/google'
+const font = IBM_Plex_Mono({
+    subsets: ['latin', 'cyrillic'],
+    weight: ['300', '400'],
+})
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <head />
+            <body
+                className={cx(
+                    'dark',
+                    'bg-gray-900 text-gray-200',
+                    font.className,
+                )}
+            >
+                {children}
+            </body>
+        </html>
+    )
 }
